ab60a85dc947b3d5a4a47f9df0076b4215239b49
Kozo Nakagawa <sugakate@gmail.com>
2026-05-08 16:04:15 +0900

    fix pwa icon manifest

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,39 @@
-const CACHE_NAME = "hotsuma-memorizer-v1";
+const CACHE_NAME = "hotsuma-memorizer-v2";
 
 const CORE_FILES = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.webmanifest",
-  "./icons/hotsuma.svg",
-  "./data/aya-list.json"
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/manifest.webmanifest",
+  "/icons/hotsuma-icon.png",
+  "/data/aya-list.json"
 ];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(CORE_FILES);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_FILES))
   );
-
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
+    caches.keys().then((keys) =>
+      Promise.all(
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      );
-    })
+      )
+    )
   );
-
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      return fetch(event.request).then((networkResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
     })
   );
 });
